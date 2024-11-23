821fa90d52911da5790ca7b66e53515107bf0c9e
Mkaif-Qureshi <quremkaif7@gmail.com>
2024-11-24 01:39:52 +0530

    minor code fix

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_origins=["*"],  # Change for specific domains in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
